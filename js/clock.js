d3a4395cf45306a0c3cdd92c3814c434adf9f0fb
amh6281 <amh6281@naver.com>
2021-11-16 16:23:03 +0900

    Feat: 현재 시간 기능 추가
    현재 시간을 나타내는 기능을 추가하면서 "01:05:13" 처럼 0으로 시작하기위해 padStart 함수를 사용했다.

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); // 새로고침해도 바로 실행됨.
+setInterval(getClock, 1000);
