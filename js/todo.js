56e20285b926249c00c2538cf047ed301c325a34
amh6281 <amh6281@naver.com>
2021-11-18 16:50:12 +0900

    Feat:  로딩 후 todo 불러오기 2021.11.18
    JSON.stringify(todos)를 사용하여 localStorage에 todos를 string으로 저장한다.
    JSON.parse를 사용하여 string을 JavaScript가 이해할 수 있는 array로 저장한다.
    forEach는 array의 각 item에 대해 function을 실행한다.

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
-const todos = [];
+const TODOS_KEY = "todos";
+
+let todos = [];
 
 function saveTodos() {
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
 
 function deleteTodo(e) {
@@ -35,5 +37,14 @@ function handleTodoSubmit(e) {
 
 todoForm.addEventListener("submit", handleTodoSubmit);
 
-//todos 배열에 newTodo를 push하고 localStorage에 저장.
-//localStorage는 array를 저장하기위해 JSON.stringify를 사용
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
+
+//JSON.stringify(todos)를 사용하여 localStorage에 todos를 string으로 저장
+// JSON.parse를 사용하여 string을 JavaScript가 이해할 수 있는 array로 저장
+// forEach는 array의 각 item에 대해 function을 실행
