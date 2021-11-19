b220ca4b1f3fad6459776e5b59c9321a432762f0
amh6281 <amh6281@naver.com>
2021-11-19 15:18:02 +0900

    Feat: 날씨 가져오기 2021.11.19
    날씨 api를 가져와 url 변수에 저장하고, 저장한 url을 fetch 함수로 호출한다.
    fetch 함수의 호출 순서는 먼저 response를 처리하고 응답을 JSON 형태로 파싱한다.
    JSON 형태로 파싱한 후 마지막으로 JSON을 출력한다.

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,17 @@
+const API_KEY = "67c557fdaf4b16dc524c060ed137b9e3";
+
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main}/${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
